c60a11022c023ea2592eaca5514275830ac84140
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2023-05-03 02:06:24 +0200

    fix: :label: Add mypy type for strenum

--- src/strenum/__init__.pyi
+++ src/strenum/__init__.pyi
@@ -1,7 +1,7 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #  MIT License                                                                         +
 #                                                                                      +
-#  Copyright (c) 2022 roberto.prevato@gmail.com                                        +
+#  Copyright (c) 2019 James C Sinclair                                                 +
 #                                                                                      +
 #  Permission is hereby granted, free of charge, to any person obtaining a copy        +
 #  of this software and associated documentation files (the "Software"), to deal       +
@@ -19,7 +19,63 @@
 #  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER              +
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,       +
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE       +
-#  SOFTWARE                                                                            +
+#  SOFTWARE.                                                                           +
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-"""Stubs for 'openapidocs' for vigenere_api package."""
+import enum
+from typing import Any
+
+
+class StrEnum(str, enum.Enum):
+    def __str__(self) -> str: ...
+
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class LowercaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class UppercaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class CamelCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class PascalCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class KebabCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class SnakeCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class MacroCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class CamelSnakeCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class PascalSnakeCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class SpongebobCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class CobolCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
+
+class HttpHeaderCaseStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, *_: Any) -> str: ...
