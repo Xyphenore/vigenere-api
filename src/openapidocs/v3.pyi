18a16a0df7c07ea256dd5ac75308423a814f38b7
DAVID Axel <axel.david@etu.univ-amu.fr>
2023-05-03 02:03:07 +0200

    fix: :page_facing_up: Fix License of files

--- src/openapidocs/v3.pyi
+++ src/openapidocs/v3.pyi
@@ -5,29 +5,36 @@ https://swagger.io/specification/.
 """
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  Vigenere-API                                                                        +
-#  Copyright (C) 2023 Axel DAVID                                                       +
+#  MIT License                                                                         +
 #                                                                                      +
-#  This program is free software: you can redistribute it and/or modify it under       +
-#  the terms of the GNU General Public License as published by the Free Software       +
-#  Foundation, either version 3 of the License, or (at your option) any later version. +
+#  Copyright (c) 2022 roberto.prevato@gmail.com                                        +
 #                                                                                      +
-#  This program is distributed in the hope that it will be useful, but WITHOUT ANY     +
-#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR       +
-#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.          +
+#  Permission is hereby granted, free of charge, to any person obtaining a copy        +
+#  of this software and associated documentation files (the "Software"), to deal       +
+#  in the Software without restriction, including without limitation the rights        +
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell           +
+#  copies of the Software, and to permit persons to whom the Software is               +
+#  furnished to do so, subject to the following conditions:                            +
 #                                                                                      +
-#  You should have received a copy of the GNU General Public License along with        +
-#  this program.  If not, see <https://www.gnu.org/licenses/>.                         +
+#  The above copyright notice and this permission notice shall be included in all      +
+#  copies or substantial portions of the Software.                                     +
+#                                                                                      +
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR          +
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,            +
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE         +
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER              +
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,       +
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE       +
+#  SOFTWARE                                                                            +
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 from abc import ABC
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any
-from typing import Optional
-from typing import Union
+from typing import Any, Optional, Union
 
-from openapidocs.common import OpenAPIElement
-from openapidocs.common import OpenAPIRoot
+from openapidocs.common import OpenAPIElement, OpenAPIRoot
+
 
 class ParameterLocation(Enum):
     QUERY = "query"
